a0cdb85fd8b0e58629d81d5b1877ee952d729081
gabrielezigurella <mantainer@git.zigurella.dev>
2024-04-25 12:34:37 +0200

    Add better usage and options with argparse, support file input, file output, debug prints and arguments type definition

--- lambda.py
+++ lambda.py
@@ -1,72 +1,103 @@
-import os, re, sys
-from typing import Any
+import datetime
+import os, sys
+import argparse
+import traceback
+from evaluator import build_lambda_head, build_lambda_body, build_lambda_args
+from pydoc import locate
+from typing import Any, TextIO
 
 
-def build_lambda_head(argv_size: int) -> str:
-    lmbd = "lambda "
-    for idx in range(argv_size):
-        lmbd += f"_arg{idx}"
-        if idx != argv_size - 1:
-            lmbd += ","
-    lmbd += ":"
-    return lmbd
+def get_current_year():
+    return datetime.datetime.now().year.real
 
 
-def build_lambda_body(expr: str, argv_size: int) -> str:
-    for idx in range(argv_size):
-        replaced_arg = f"\\${idx + 1}"
-        replace_value = f"_arg{idx}"
-        expr = re.sub(replaced_arg, replace_value, expr)
-    return expr
+def getopt(argv):
+    return parser.parse_args(argv)
 
 
-def build_lambda_args(argv: list[Any], argv_size: int) -> str:
-    lmbd = "("
-    for idx in range(argv_size):
-        lmbd += f"{argv[idx]}"
-        if idx != argv_size - 1:
-            lmbd += ","
-    lmbd += ")"
-    return lmbd
+parser = argparse.ArgumentParser(
+    prog='lambda',
+    description='''description:
+    Execute python lambda's body expression on positional arguments or 
+    operations on file with arrays and list comprehension.
+
+    Example on positional arguments: lambda \"$1 ** $2\" arg1 arg2
+    Example on list arguments: lambda \"$i + 2 for $i in $@\" arg1 arg2 ... argN''',
+    formatter_class=argparse.RawTextHelpFormatter,
+    epilog=f'Author: Gabriele Zigurella © {get_current_year()}')
+
+parser.add_argument('expr', metavar='EXPR', type=str, nargs=1,
+                    help='Lambda expression or list comprehension expression to be applied')
+parser.add_argument('args', metavar='$A', type=str, nargs='?',
+                    help='Positional Arguments to be used during the lambda invocation')
+parser.add_argument('-D', '--debug', dest='debug', action='store_true',
+                    help='It will output traceback errors on execution failure.')
+parser.add_argument('-dt', '--dtype=', dest='dtype', action='store',
+                    default='str', nargs=1,
+                    help='Define the type of the positional arguments, if omitted they will be treated as strings.')
+parser.add_argument('-f', '--filepath=', dest='source', type=argparse.FileType('r'),
+                    nargs='?', default=sys.stdin,
+                    help='Define the File from witch to read, each lines will be parsed into a list argument.')
+parser.add_argument('-d', '--delim=', dest='delimiter', type=str, nargs='?',
+                    help='''Combined with flag -f (or --filepath) allows to split each file line on given delimiter
+                         and treat each splitted value as an argument.''')
+parser.add_argument('-o', '--output=', dest='output', type=argparse.FileType('w'),
+                    nargs='?', default=sys.stdout,
+                    help='Define the File from witch to read, each lines will be parsed into a list argument.')
 
 
-def main(expr: str, argv: list[Any]) -> int:
+def main(options: argparse.Namespace) -> int:
     try:
+        __lambda_expr__ = parsed_cmd.expr[0]
+        argv = lambda_input(options)
+        res: Any
         argv_size = len(argv)
-        __lambda__ = eval(build_lambda_head(argv_size) + build_lambda_body(expr, argv_size))
-        __args__ = build_lambda_args(argv, argv_size)
-        res = __lambda__(*argv)
-        print(res)
+        if not ("$@" in __lambda_expr__):
+            __lambda__ = eval(
+                build_lambda_head(argv_size, False) + build_lambda_body(__lambda_expr__, argv_size, False))
+            __args__ = build_lambda_args(argv, argv_size)
+            res = __lambda__(*argv)
+        else:
+            list_comprehension = build_lambda_body(__lambda_expr__, argv_size, True)
+            res = eval(list_comprehension)
+        output(res, options.output)
     except Exception as e:
-        print(e)
-        print(os.strerror(32))
+        if hasattr(options, 'debug'):
+            traceback.print_exc()
+        print(os.strerror(32), e)
         return 32
     return 0
 
 
-def usage():
-    print(f"""
-    > lambda \"$1 ** $2\" arg1 arg2
-    > lambda \"k + 2 for k in $@\" arg1...argN
-    
-    ### OPTIONAL FLAGS ###
-        -h, --help | show this usage message for guidance
-        
-    ### LAMBDA ARGUMENTS ###
-    Lambda uses positional arguments $1, $2, $N to evaluate and apply the python expression on the given input.
-    If not explicit cast is given all lambda parameters are tought to be string type.
-    
-    Use $@ to apply the lambda to all given parameters, considering them part of an Array of elements.
-    """)
+def lambda_input(options: argparse.Namespace):
+    args: Any
+    if options.source != sys.stdin:
+        with open(options.source.name) as file:
+            options.args = [line.strip() for line in file]
+            if hasattr(options, "delimiter"):
+                def flatten_concatenation(arg_matrix):
+                    flat_list = []
+                    for row in arg_matrix:
+                        flat_list += row.split(options.delimiter)
+                    return flat_list
+                options.args = flatten_concatenation(options.args)
+    if not hasattr(options, 'dtype'):
+        args = options.args
+    else:
+        dtype: object = locate(options.dtype[0])
+        args = [dtype(arg) for arg in options.args]
+    return args
+
+
+def output(res: Any, out: TextIO):
+    if out == sys.stdout:
+        print(res)
+    else:
+        with open(out.name, 'w') as file:
+            file.write(f"{res}")
 
 
 if __name__ == '__main__':
-    if "-h" in sys.argv or "--help" in sys.argv:
-        usage()
-        exit(0)
-    if len(sys.argv) < 2:
-        print("INVALID ARGUMENTS ERROR: Please invoke 'lambda -h' or 'lambda --help' to see the correct program usage.")
     __program__ = sys.argv[0]
-    __lambda_expr__ = sys.argv[1]
-    __lamda_args__ = sys.argv[2:]
-    exit(main(__lambda_expr__, __lamda_args__))
+    parsed_cmd = getopt(sys.argv[1:])
+    exit(main(options=parsed_cmd))
